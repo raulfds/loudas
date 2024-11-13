970ca0f840136e578b0801bd929d8b48caebd76a
raulfds <96803581+raulfds@users.noreply.github.com>
2024-11-13 14:44:20 -0300

    int.1.02

--- loudas/app/api/auth/[...nextauth]/route.ts
+++ loudas/app/api/auth/[...nextauth]/route.ts
@@ -1,9 +1,9 @@
-import NextAuth from "next-auth"
+import NextAuth, { NextAuthOptions } from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
 import { MongoDBAdapter } from "@next-auth/mongodb-adapter"
 import clientPromise from "@/lib/mongodb"
 
-const handler = NextAuth({
+export const authOptions: NextAuthOptions = {
   adapter: MongoDBAdapter(clientPromise),
   providers: [
     GoogleProvider({
@@ -19,6 +19,8 @@ const handler = NextAuth({
       return session;
     },
   },
-})
+}
+
+const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
